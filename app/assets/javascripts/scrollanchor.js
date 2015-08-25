9b58d95c934d6d0927fad3aacbde1b4832c323b6
Thomas Lixing Yang <tboneyang@gmail.com>
2015-08-25 15:04:13 -0400

    updated landing page to have looping video and removed cloud animations in about page

--- app/assets/javascripts/scrollanchor.js
+++ app/assets/javascripts/scrollanchor.js
@@ -1,5 +1,32 @@
 var ready = function() {
 
+  var vid = document.getElementById("bgvid");
+  var pauseButton = document.getElementById("pauseb");
+
+  function vidFade() {
+    vid.classList.add("stopfade");
+  }
+
+  vid.addEventListener('ended', function()
+  {
+  // only functional if "loop" is removed
+  vid.pause();
+  // to capture IE10
+  vidFade();
+  });
+
+
+  pauseButton.addEventListener("click", function() {
+    vid.classList.toggle("stopfade");
+    if (vid.paused) {
+      vid.play();
+      pauseButton.innerHTML = "PAUSE";
+    } else {
+      vid.pause();
+      pauseButton.innerHTML = "PAUSED";
+    }
+  })
+
   $(function() {
     $('a[href*=#]:not([href=#])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
